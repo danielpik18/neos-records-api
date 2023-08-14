af65fce06a294f996c0c9696d58e6533ecb1529c
unknown <danielmomusic@gmail.com>
2023-08-14 17:10:00 -0500

    Last tweaks before commit

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://neos-records.onrender.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -34,7 +34,7 @@ app.use(cookieParser())
 /*app.use(morgan('dev'))*/
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://neos-records.onrender.com'],
     credentials: true
 }))
 app.use(express.json())
